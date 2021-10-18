8f1db5861d1a52aabf11a2e4070c8d3e06532e27
pratham <prathamghule3@gmail.com>
2021-10-18 16:25:42 +0530

    insert and help table methods added

--- DBMS_COURSE_PROJECT/src/com/company/Main.java
+++ DBMS_COURSE_PROJECT/src/com/company/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class Main {
     private static void create_table(String[] tokens){
-        //create table emp(name,char,roll,int);
+        //create table dept(name,char,roll,int);
         String[] data=tokens[2].split("\\(");
         String resString=data[0];
         String[] attributes=data[1].split(",");
@@ -38,22 +38,83 @@ public class Main {
         }
     }
 
-    private static void insert_entries(String[] tokens) {
+    private static void insert_entries(String[] tokens) throws IOException {
         //insert into emp(name,roll) values("hello",4);
+        String table_name=(tokens[2].split("\\("))[0];
+        File table=new File("src\\tables\\"+table_name+".txt");
+        FileWriter file=new FileWriter("src\\tables\\"+table_name+".txt",true);
+        if(table.exists()){
+            Writer output = new BufferedWriter(file);
+            String[] entries=(tokens[3].substring(7,tokens[3].length()-2)).split(",");
+            String resString=entries[0];
+            for(int i=1;i<entries.length;i++){
+                resString+="#"+entries[i];
+            }
+            output.write(resString+"\r\n");
+            output.close();
+            System.out.println(resString);
+            System.out.println("Values inserted successfully");
+        }
+        else
+            System.out.println("Table does not exist");
+    }
+
+    private static void describe(String[] tokens) {
+
+    }
+
+    private static void help_tables() throws IOException {
+        //help tables
+
+        File file=new File("src\\db\\schema.txt");
+        if(file.length()==0)
+            System.out.println("No tables found");
+        else{
+            FileReader fr=new FileReader(file);
+            BufferedReader br=new BufferedReader(fr);
+            System.out.println("Table Names:");
+            String line;
+            while((line=br.readLine())!=null){
+                System.out.println((line.split("#"))[0]);
+            }
+        }
     }
 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         String query;
-        do{
+        while(true){
             query=sc.nextLine();
             query=query.toLowerCase();
             String[] tokens=query.split(" ");
-            if(tokens[0].equals("create")){
+            if(tokens[0].equals("quit")){
+                break;
+            }
+            else if(tokens[0].equals("create")){
                 create_table(tokens);
             }
-            else if(tokens[0].equals("insert"))
-                insert_entries(tokens);
-        }while(!query.equals("quit"));
+            else if(tokens[0].equals("insert")){
+                try {
+                    insert_entries(tokens);
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            else if(tokens[0].equals("describe")){
+                describe(tokens);
+            }
+            else if(query.equals("help tables")){
+                try {
+                    help_tables();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            else{
+                System.out.println("Invalid Query");
+            }
+        }
     }
+
 }
