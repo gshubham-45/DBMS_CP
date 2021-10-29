fbfef4bfe7e6f1804d5c6e74d0ef2ca609c1cecd
pratham <prathamghule3@gmail.com>
2021-10-29 13:47:04 +0530

    Updated create table method to accept constraints for table

--- DBMS_COURSE_PROJECT/src/com/company/Main.java
+++ DBMS_COURSE_PROJECT/src/com/company/Main.java
@@ -6,24 +6,47 @@ import java.util.Scanner;
 
 
 public class Main {
-    private static void create_table(String[] tokens){
-        //create table dept(name,char,roll,int);
-        String[] data=tokens[2].split("\\(");
-        String resString=data[0];
+    private static void create_table(String[] tokens,String query){
+        //create table student(roll,int,check(roll>0),name,char(10),PRIMARY KEY(roll),FOREIGN KEY name REFERENCES emp(name));
+        String[] data=query.split("\\(",2);
+        String tableName=(data[0].split(" "))[2];
+        String resString=tableName;
         String[] attributes=data[1].split(",");
         int n=attributes.length;
         for(int i=0;i<n;i++){
             if(i==n-1){
-                resString+="#"+attributes[n-1].substring(0,attributes[n-1].length()-2);
+                if(attributes[i].contains("primary") || attributes[i].contains("foreign")){
+                    if(resString.charAt(resString.length()-1)!='$')
+                        resString+="$"+attributes[n-1].substring(0,attributes[n-1].length()-2);
+                    else
+                        resString+=attributes[n-1].substring(0,attributes[n-1].length()-2);
+                }
+                else
+                    resString+="#"+attributes[n-1].substring(0,attributes[n-1].length()-2);
             }
             else{
-                resString+="#"+attributes[i];
+                if(attributes[i].contains("check")){
+                    resString+="#"+attributes[i]+"$";
+                }
+                else if(attributes[i].contains("primary") || attributes[i].contains("foreign")){
+                    if(resString.charAt(resString.length()-1)!='$')
+                        resString+="$"+attributes[i];
+                    else
+                        resString+=attributes[i];
+                }
+                else{
+                    if(resString.charAt(resString.length()-1)!='$')
+                        resString+="#"+attributes[i];
+                    else
+                        resString+=attributes[i];
+                }
+
             }
         }
 
         try{
             Writer output = new BufferedWriter(new FileWriter("src\\db\\schema.txt", true));
-            File newTable=new File("src\\tables\\"+data[0]+".txt");
+            File newTable=new File("src\\tables\\"+tableName+".txt");
             boolean status=newTable.createNewFile();
             if(status){
                 output.write(resString+"\r\n");
@@ -59,8 +82,20 @@ public class Main {
             System.out.println("Table does not exist");
     }
 
-    private static void describe(String[] tokens) {
-
+    private static void describe(String[] tokens) throws IOException{
+        //describe emp
+        File file=new File("src\\tables\\"+tokens[1]+".txt");
+        if(file.exists()){
+            FileReader fr=new FileReader(file);
+            BufferedReader br=new BufferedReader(fr);
+            String line;
+            while((line=br.readLine())!=null){
+                System.out.println((line.split("#"))[0]);
+            }
+        }
+        else{
+            System.out.println("Table does not exist");
+        }
     }
 
     private static void help_tables() throws IOException {
@@ -91,7 +126,7 @@ public class Main {
                 break;
             }
             else if(tokens[0].equals("create")){
-                create_table(tokens);
+                create_table(tokens,query);
             }
             else if(tokens[0].equals("insert")){
                 try {
@@ -102,7 +137,11 @@ public class Main {
                 }
             }
             else if(tokens[0].equals("describe")){
-                describe(tokens);
+                try {
+                    describe(tokens);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
             else if(query.equals("help tables")){
                 try {
